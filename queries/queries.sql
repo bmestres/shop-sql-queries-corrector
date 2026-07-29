a3edbca20dfc4d9c64d0d65c97b1d09ecc6f3c74
bmestres <132062905+bmestres@users.noreply.github.com>
2026-07-29 13:19:02 +0200

    Remove empty line in queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,6 +1,5 @@
 
 -- 1. Llista el nom de tots els productes que hi ha en la taula producto.
-
 SELECT producto.nombre
 FROM producto;
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
